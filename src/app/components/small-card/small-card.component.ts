1f97174316888da75961625b3c868cf64416240b
carlosaugusto <109244715+carlosaugusto28@users.noreply.github.com>
2023-10-07 21:54:02 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -13,6 +13,9 @@ export class SmallCardComponent implements OnInit {
   @Input()
   cardTitle:string = "Hades 2 recebe novo trailer"
 
+  @Input()
+  Id:string ="0"
+
   constructor() { }
 
   ngOnInit(): void {
